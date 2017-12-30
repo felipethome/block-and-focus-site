37cf43f0377b74009565551cbbd13e9e607036fa
felipethome <f.o.thome@gmail.com>
2017-12-30 16:17:24 -0200

    Implement the footer with fixed height

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Footer extends React.Component {
+  static displayName = 'Footer';
+
+  static propTypes = {
+    style: PropTypes.object,
+  };
+
   render() {
     const styles = {
       container: {
         display: 'flex',
         justifyContent: 'center',
+        alignItems: 'center',
         width: '100%',
+        height: '100px',
       },
 
       textContainer: {
         fontSize: '13px',
         color: '#9E9E9E',
         textAlign: 'center',
-        margin: '40px 0 40px 0',
       },
     };
 
